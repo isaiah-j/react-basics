c51cb639bd43575a0850e298e82b86db0c16fdc3
Isaiah <greatpup1940@gmail.com>
2020-06-28 16:13:15 -0400

    Fetched from an API and changed the state

--- src/App.js
+++ src/App.js
@@ -1,38 +1,36 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function add(){
-  return 2+2
+function add() {
+	return 2 + 2;
 }
 
-class App extends Component{
-  constructor(){
-    super();
-    this.state = {
-      characters: [
-        {name: "Aang", id: 1},
-        {name: "Katara", id: 2},
-        {name: "Sokka" ,id: 3},
-        {name: "Zuko", id: 4},
-
-      ]
-    }
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			characters: []
+		};
   }
-  render(){
-    return (
-      <div className="App">
-        <header className="App-header">
-          {this.state.characters.map(el => {
-            return <h1 key={el.id}>{el.name}</h1>
-          })}
+  
 
-     
-        </header>
-      </div>
-    );
+  componentDidMount(){
+    fetch("https://swapi.dev/api/people/").then(res => res.json()).then(data => this.setState({characters: data.results}))
+
   }
+
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					{this.state.characters.map((el) => {
+						return <h1 key={el.id}>{el.name}</h1>;
+					})}
+				</header>
+			</div>
+		);
+	}
 }
-
 
 export default App;
