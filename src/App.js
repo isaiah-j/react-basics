4cf5d19bed7bb7855890b54f45720d703b53c8ed
Isaiah <greatpup1940@gmail.com>
2020-06-28 23:58:04 -0400

    Creaated the Card component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { CardList } from './Components/Card-List/Card-List-Component';
 
 function add() {
 	return 2 + 2;
@@ -12,21 +13,21 @@ class App extends Component {
 		this.state = {
 			characters: []
 		};
-  }
-  
+	}
 
-  componentDidMount(){
-    fetch("https://swapi.dev/api/people/").then(res => res.json()).then(data => this.setState({characters: data.results}))
-
-  }
+	componentDidMount() {
+		fetch('https://swapi.dev/api/people/')
+			.then((res) => res.json())
+			.then((data) => this.setState({ characters: data.results }));
+	}
 
 	render() {
 		return (
 			<div className="App">
 				<header className="App-header">
-					{this.state.characters.map((el) => {
-						return <h1 key={el.id}>{el.name}</h1>;
-					})}
+					<CardList characters={this.state.characters}>
+						
+					</CardList>
 				</header>
 			</div>
 		);
---
+++ src/Components/Card-List/Card-List-Component.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Card-List-Styles.css';
+
+import {Card} from "../Card/card.component"
+
+export const CardList = (props) => {
+	return (
+		<div className="card-list">
+			{props.characters.map((el) => {
+				return <Card character={el} ></Card>;
+			})}
+		</div>
+	);
+};
